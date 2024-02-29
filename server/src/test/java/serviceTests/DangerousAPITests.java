f8421e6127fb194e861414f088a1c14fc7b4956a
ZapTerra <43815664+ZapTerra@users.noreply.github.com>
2024-02-28 20:32:13 -0700

    Test tests

---
+++ server/src/test/java/serviceTests/DangerousAPITests.java
@@ -0,0 +1,62 @@
+package serviceTests;
+
+import org.junit.jupiter.api.Test;
+
+public class DangerousAPITests {
+    @Test
+    void murder(){
+        assert true;
+    }
+    @Test
+    void innocence(){
+        assert false;
+    }
+    @Test
+    void pain(){
+        assert true;
+    }
+    @Test
+    void rats(){
+        assert false;
+    }
+    @Test
+    void tea(){
+        assert true;
+    }
+    @Test
+    void biscuits(){
+        assert false;
+    }
+    @Test
+    void ennui(){
+        assert true;
+    }
+    @Test
+    void dissonance(){
+        assert false;
+    }
+    @Test
+    void rapture(){
+        assert true;
+    }
+    @Test
+    void psychosis(){
+        assert false;
+    }
+    @Test
+    void psychotic(){
+        assert true;
+    }
+    @Test
+    void psychokinesis(){
+        assert false;
+    }
+    @Test
+    void psychokinetic(){
+        assert true;
+    }
+    @Test
+    void psionic(){
+        assert false;
+    }
+}
