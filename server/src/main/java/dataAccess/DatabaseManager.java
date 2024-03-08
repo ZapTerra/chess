30651bc5828dd2ec57633fd0364ba515f3d46048
ZapTerra <43815664+ZapTerra@users.noreply.github.com>
2024-03-08 00:04:30 -0700

    maybe fix 2

--- server/src/main/java/dataAccess/DatabaseManager.java
+++ server/src/main/java/dataAccess/DatabaseManager.java
@@ -37,6 +37,7 @@ public class DatabaseManager {
     static void createDatabase() throws DataAccessException {
         try {
             var statement = "CREATE DATABASE IF NOT EXISTS " + databaseName;
+
             var conn = DriverManager.getConnection(connectionUrl, user, password);
             try (var preparedStatement = conn.prepareStatement(statement)) {
                 preparedStatement.executeUpdate();
